cb873c4ecdada41038ffb3f59c7e16adaa767c64
XxDevilsCloverxX <silasrodriguez02@gmail.com>
2023-04-26 00:22:05 -0500

    Update Comments

--- movie_rec_proj/Movie.java
+++ movie_rec_proj/Movie.java
@@ -1,8 +1,8 @@
 package movie_rec_proj;
-import java.util.ArrayList;
+import java.util.ArrayList; // import the ArrayList class for dynamic arrays -> lists that grow and shrink in size as needed -> used to create a database of movies that can be accessed by all users
 
 /**
- * @author Silas Rodriguez, 
+ * @authors Silas Rodriguez, Katrina Hellmann, Michael Gibich
  * @assignment CS2365 OOP
  * @date 4/25/2023
  * @purpose This class is used to create movie objects that can be added to a database and accessed by users
@@ -10,66 +10,92 @@ import java.util.ArrayList;
 public class Movie{
     //create an array list that acts as a database for all movies can be accessed by all users in the package using get_movie_list() method below -> Could be modified by a future object like "movie_database"
     protected static ArrayList<Movie> movie_list = new ArrayList<Movie>();
-    // movie name and year
-    private String movie_name;  // required
-    private int movie_year;     // required
-    private char movie_rating;  // required
-    private String movie_genre; // required
+    //private variables for movie object
+    private String movie_name;  // required movie name
+    private int movie_year;     // required movie year
+    private char movie_rating;  // required movie rating
+    private String movie_genre; // required movie genre
     
-    // constructor for required fields
+    /*
+     * Constructor for movie object with all required parameters for the movie object
+     */
     public Movie(String movie_name, int movie_year, char movie_rating, String movie_genre){
         this.movie_name = movie_name.toUpperCase(); // convert to uppercase to make searching easier
-        this.movie_year = movie_year;
+        this.movie_year = movie_year;   //set the movie year as an integer, because years aren't decimals
         this.movie_rating = Character.toUpperCase(movie_rating);    // convert to uppercase to make searching easier
         this.movie_genre = movie_genre.toUpperCase();   // convert to uppercase to make searching easier
         movie_list.add(this);
     }
+    /*
+     * Default constructor: throws an exception if the user tries to create a movie object without all required parameters
+     */
     public Movie (){
         throw new IllegalArgumentException("Movie must have a name, year, rating, and genre");
     }
-    
-    // set movie name
+    /*
+     * setter for the movie name
+     */
     public void setMovieName(String movie_name){
         this.movie_name = movie_name;
     }
-    // set movie year
+    /*
+     * setter for the movie year
+     */
     public void setMovieYear(int movie_year){
         this.movie_year = movie_year;
     }
-    // set movie rating
+    /*
+     * setter for the movie rating
+     */
     public void setMovieRating(char movie_rating){
         this.movie_rating = movie_rating;
     }
-    // set movie genre
+    /*
+     * setter for the movie genre
+     */
     public void setMovieGenre(String movie_genre){
         this.movie_genre = movie_genre;
     }
-    // get movie name
+    /*
+     * getter for the movie name
+     */
     public String getMovieName(){
         return this.movie_name;
     }
-    // get movie year
+    /*
+     * getter for the movie year
+     */
     public int getMovieYear(){
         return this.movie_year;
     }
-    // get movie rating
+    /*
+     * getter for the movie rating
+     */
     public char getMovieRating(){
         return this.movie_rating;
     }
-    // get movie genre
+    /*
+     * getter for the movie genre
+     */
     public String getMovieGenre(){
         return this.movie_genre;
     }
-    // get movie list
+    /*
+     * getter for the movie list
+     */
     public static ArrayList<Movie> getMovieList(){
         return movie_list;
     }
-    
-    // get movie list size
+    /*
+     * getter for the movie list size
+     */
     public static int getMovieListSize(){
         return movie_list.size();
     }
 
+    /*
+     * toString method for the movie object -> prints out all the information about the movie in a pretty format
+     */
     @Override
     public String toString(){
         return "Movie Name: " + this.movie_name + "\nMovie Year: " + this.movie_year + "\nMovie Rating: " + this.movie_rating + "\nMovie Genre: " + this.movie_genre + "\n";
--- movie_rec_proj/User.java
+++ movie_rec_proj/User.java
@@ -1,8 +1,13 @@
-package movie_rec_proj;
+package movie_rec_proj; 
 
-import java.util.ArrayList;
+import java.util.ArrayList; // used for a list of admin users 
 import javax.swing.*;
-// Rules accounts must follow: settable and gettable user name and age
+/**
+ * @authors Silas Rodriguez, Katrina Hellmann, Michael Gibich
+ * @assignment CS2365 OOP
+ * @date 4/25/2023
+ * @purpose This interface is used to enforce user objects to implement certain methods that are required for all users and subclasses to have
+ */
 interface Account {
     // every user has a name and an age that must be set and can be retrieved
     public abstract void setUserName(String userName);
@@ -13,72 +18,102 @@ interface Account {
     public abstract void callRecommendation();
 }
 
+/**
+ * @authors Silas Rodriguez, Katrina Hellmann, Michael Gibich
+ * @assignment CS2365 OOP
+ * @date 4/25/2023
+ * @children Admin, Child, Senior, Adult
+ * @purpose This class is used to create user objects and appropriate subclasses that can interface with the recommendation object
+ */
 public class User implements Account {
-    // users have a name and an age and Admin status
-    private String userName;
-    private int userAge;
-    private boolean isAdmin;
+    // private variables for user object
+    private String userName;    // required user name
+    private int userAge;    // required user age
+    private boolean isAdmin;    // required admin status
 
-    // constructor with all fields
+    /*
+     * Constructor for user object with all required parameters for the user object
+     */
     public User(String userName, int userAge, boolean isAdmin){
-        this.userName = userName;
-        this.userAge = userAge;
-        this.isAdmin = isAdmin;
+        this.userName = userName;   // set the user name
+        if (userAge < 0){
+            this.userAge = 0;   // if the user enters a negative age, set the age to 0
+        }
+        this.userAge = userAge; // set the user age
+        this.isAdmin = isAdmin; // set the admin status
     }
-    // constructor with only name and age
+    /*
+     * Constructor for user object with all required parameters for the user object except for admin status
+     */
     public User(String userName, int userAge){
-        this(userName, userAge, false);
+        this(userName, userAge, false); // call the other constructor with the admin status set to false
     }
-    // default constructor    
+    /*
+     * Default constructor: throws an exception if the user tries to create a user object without all required parameters
+     */    
     public User(){
         throw new IllegalArgumentException("User must have a name and an age");
     }
 
-    // set user name
+    /*
+     * setter for user name
+     */
     @Override
     public void setUserName(String userName){
         this.userName = userName;
     }
-    // set user age
+    /*
+     * setter for user age
+     */
     @Override
     public void setUserAge(int userAge){
         this.userAge = userAge;
     }
 
-    // get user name
+    /*
+     * getter for user name
+     */
     @Override
     public String getUserName(){
         return this.userName;
     }
-    // get user age
+    /*
+     * getter for user age
+     */
     @Override
     public int getUserAge(){
         return this.userAge;
     }
-    // get admin status
+    /*
+     * getter for admin status
+     */
     @Override
     public boolean getIsAdmin(){
         return this.isAdmin;
     }
 
+    /*
+     * method for calling the recommendation object and getting a recommendation
+     */
     @Override
     public void callRecommendation() {
-        // JAVA FX CODE HERE
-        String name;
-        String genre;
-        String rating;
-        String minYear;
-        String maxYear;
+        // JAVA FX CODE HERE -- THIS IS JUST A PLACEHOLDER if we decide to use JavaFX
+        String name;    // name of the movie
+        String genre;   // genre of the movie
+        String rating;  // rating of the movie
+        String minYear; // minimum year of the movie
+        String maxYear; // maximum year of the movie
 
         // create a new recommendation object
-        Recommendation rec = new Recommendation(this);
+        Recommendation rec = new Recommendation(this);  // pass the user object to the recommendation object
         
-        JTextField input1 = new JTextField();
-        JTextField input2 = new JTextField();
-        JTextField input3 = new JTextField();
-        JTextField input4 = new JTextField();
-        JTextField input5 = new JTextField();
+        JTextField input1 = new JTextField();   // create text fields for user input
+        JTextField input2 = new JTextField();   // create text fields for user input
+        JTextField input3 = new JTextField();   // create text fields for user input
+        JTextField input4 = new JTextField();   // create text fields for user input
+        JTextField input5 = new JTextField();   // create text fields for user input
 
+        // create a message for the user to enter a search query    -> this formats the JOptionPane to look nice
         Object[] message = {
             "Movie Name:", input1,
             "Movie Genre:", input2,
@@ -87,15 +122,18 @@ public class User implements Account {
             "Movie Max Year:", input5
         };
 
+        // create a JOptionPane for the user to enter a search query
         int option = JOptionPane.showConfirmDialog(null, message, "Enter a search query", JOptionPane.OK_CANCEL_OPTION);
 
+        // if the user clicks OK, get the user input and store it in the appropriate variables
         if (option == JOptionPane.OK_OPTION) {
-            name = input1.getText();
-            genre = input2.getText();
-            rating = input3.getText();
-            minYear = input4.getText();
-            maxYear = input5.getText();
+            name = input1.getText();    // get the user input for movie name
+            genre = input2.getText();   // get the user input for movie genre
+            rating = input3.getText();  // get the user input for movie rating
+            minYear = input4.getText(); // get the user input for movie min year
+            maxYear = input5.getText(); // get the user input for movie max year
         }
+        // if the user clicks cancel, return
         else{
             return;
         }
@@ -103,18 +141,21 @@ public class User implements Account {
         // update the recommendation object
         rec.setQueryName(name);
         rec.setQueryGenre(genre);
+        // try to set the rating, if the user enters an invalid rating, set the rating to 0
         try{
             rec.setQueryRating(rating.toCharArray()[0]);
         }
         catch (ArrayIndexOutOfBoundsException e){
             rec.setQueryRating('0');
         }
+        // try to set the min year, if the user enters an invalid year, set the year to 0
         try {
             rec.setQueryYearMin(Integer.parseInt(minYear));
         }
         catch (NumberFormatException e){
             rec.setQueryYearMin(0);
         }
+        // try to set the max year, if the user enters an invalid year, set the year to 0
         try{
             rec.setQueryYearMax(Integer.parseInt(maxYear));
         }
@@ -127,32 +168,48 @@ public class User implements Account {
         rec.printRecommendedMovies();
     }
 
-    // method for automatically creating a user object of the appropriate subclass
+    /*
+     * method for automatically creating a user object of the appropriate subclass based on the user's age
+     */
     public static User CreateAppropriateSubclass(String user_name, int user_age, boolean is_admin){
         // if the user is an admin, create an admin object and return it -> Returns an error without uname and age
         if (is_admin){
             return new Admin(user_name, user_age);
         }
+        // not an admin, try to create a child object, if that fails, try to create an adult object, if that fails, create a senior object by default
         try{
             return new Child(user_name, user_age);
         }
+        // IllegalArgumentException is thrown if the user is not a child -> by subclass hierarchy, this means the user might be an adult
         catch (IllegalArgumentException e){
             try{
                 return new Adult(user_name, user_age);
             }
+            // IllegalArgumentException is thrown if the user is not an adult -> by subclass hierarchy, this means the user must be a senior
             catch (IllegalArgumentException f){
                 return new Senior(user_name, user_age);
             }
         }
     }
-    // method for automatically creating a user object of the appropriate subclass
+    /*
+     * method for automatically creating a user object of the appropriate subclass based on the user's age (overloads the previous method)
+     */
     public static User CreateAppropriateSubclass(String user_name, int user_age){
-        return CreateAppropriateSubclass(user_name, user_age, false);
+        return CreateAppropriateSubclass(user_name, user_age, false);   // default admin status is false, but can reuse previous code
     }
 }
 
+/**
+ * @authors Silas Rodriguez, Katrina Hellmann, Michael Gibich
+ * @assignment CS 2365 OOP Project
+ * @created 4/25/2023
+ * @parent User
+ * @purpose Child class for User class hierarchy, inherits from User, verify that the user is a child
+ */
 class Child extends User{
-    // constructor
+    /*
+     * constructor for child class that calls the super constructor and verifies that the user is a child
+     */
     public Child(String user_name, int user_age){
         super(user_name, user_age);
         if (user_age >= 18){
@@ -160,9 +217,17 @@ class Child extends User{
         }
     }
 }
-
+/**
+ * @authors Silas Rodriguez, Katrina Hellmann, Michael Gibich
+ * @assignment CS 2365 OOP Project
+ * @created 4/25/2023
+ * @parent User
+ * @purpose Child class for User class hierarchy, inherits from User, verify that the user is an adult
+ */
 class Adult extends User{
-    // constructor
+    /*
+     * constructor for adult class that calls the super constructor and verifies that the user is an adult
+     */
     public Adult(String user_name, int user_age){
         super(user_name, user_age);
         if (user_age < 18 || user_age >= 65){
@@ -170,9 +235,17 @@ class Adult extends User{
         }
     }
 }
-
+/**
+ * @authors Silas Rodriguez, Katrina Hellmann, Michael Gibich
+ * @assignment CS 2365 OOP Project
+ * @created 4/25/2023
+ * @parent User
+ * @purpose Child class for User class hierarchy, inherits from User, verify that the user is a senior
+ */
 class Senior extends User{
-    // constructor
+    /*
+     * constructor for senior class that calls the super constructor and verifies that the user is a senior
+     */
     public Senior(String user_name, int user_age){
         super(user_name, user_age);
         if (user_age < 65){
@@ -180,11 +253,19 @@ class Senior extends User{
         }
     }
 }
-
+/**
+ * @authors Silas Rodriguez, Katrina Hellmann, Michael Gibich
+ * @assignment CS 2365 OOP Project
+ * @created 4/25/2023
+ * @parent User
+ * @purpose Child class for User class hierarchy, inherits from User, elevates the user to admin status
+ */
 class Admin extends User{
     // private list of Admin users that can be accessed within the Admin class and dynamically grows
     private static ArrayList<Admin> Admin_list = new ArrayList<Admin>();
-    // constructor
+    /*
+     * constructor for admin class that calls the super constructor and elevates the user to admin status
+     */
     public Admin(String user_name, int user_age){
         super(user_name, user_age, true);
         Admin_list.add(this);
